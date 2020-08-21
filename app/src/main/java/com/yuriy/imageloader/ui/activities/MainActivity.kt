f74c3ccb727c15302dc15d524c9cb345dd79d330
yuriy-burkut <burkut89@gmail.com>
2020-08-21 16:20:30 +0300

    Created adapter for ViewPager, empty fragments for searching images and favorites, ViewPager connected to TabLayout

--- app/src/main/java/com/yuriy/imageloader/ui/activities/MainActivity.kt
+++ app/src/main/java/com/yuriy/imageloader/ui/activities/MainActivity.kt
@@ -8,9 +8,6 @@ import com.yuriy.imageloader.R
 import com.yuriy.imageloader.ui.adapters.MainTabsPagerAdapter
 import kotlinx.android.synthetic.main.activity_main.*
 
-const val SELECTED_TAB_POSITION = "selected_tab_position"
-
-
 class MainActivity : AppCompatActivity() {
     private val viewPagerAdapter by lazy {
         MainTabsPagerAdapter(this)
@@ -25,18 +22,6 @@ class MainActivity : AppCompatActivity() {
 
         TabLayoutMediator(tab_layout, view_pager) { tab, position ->
             tab.text = tabTitles[position]
-            view_pager.currentItem = position
         }.attach()
-
-        if (savedInstanceState != null) {
-            view_pager.currentItem = savedInstanceState.getInt(SELECTED_TAB_POSITION)
-        } else {
-            view_pager.currentItem = 0
-        }
-    }
-
-    override fun onSaveInstanceState(outState: Bundle) {
-        outState.putInt(SELECTED_TAB_POSITION, tab_layout.selectedTabPosition)
-        super.onSaveInstanceState(outState)
     }
 }
