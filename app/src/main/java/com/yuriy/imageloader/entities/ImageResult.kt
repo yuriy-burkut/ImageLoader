595e3a3ac38780b826394a80206864bd0bada220
yuriy-burkut <burkut89@gmail.com>
2020-08-26 13:08:25 +0300

    created callbacks and iu methods for checkbox selecting, savind selected state on orientation change

--- app/src/main/java/com/yuriy/imageloader/entities/ImageResult.kt
+++ app/src/main/java/com/yuriy/imageloader/entities/ImageResult.kt
@@ -3,9 +3,9 @@ package com.yuriy.imageloader.entities
 import com.squareup.moshi.JsonClass
 
 @JsonClass(generateAdapter = true)
-data class ImageResult (
-    val media : List<Media>,
-    val title : String,
+data class ImageResult(
+    val media: List<Media>,
+    val title: String,
     val id: String
 )
 
